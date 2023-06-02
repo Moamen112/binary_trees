d4f657763a2d0013d7cbe1965a788781d7bfdfba
Moamen Shehab <memo.shehab30@gmail.com>
2023-06-02 02:52:06 +0200

    Fixing errors

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -14,12 +14,12 @@ binary_tree_t *create_binary_tree_node(binary_tree_t *parent, int value)
 
 	new_node = malloc(sizeof(binary_tree_t));
 	if (new_node == NULL)
-		return NULL;
+		return (NULL);
 
 	new_node->data = value;
 	new_node->left = NULL;
 	new_node->right = NULL;
 	new_node->parent = parent;
 
-	return new_node;
+	return (new_node);
 }
